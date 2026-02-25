23fbfae85fe3b2bc276d29254cff13e0fe0732da
Muhammad Ahsen <ahsen.shiekh23@gmail.com>
2026-02-25 16:31:04 +0500

    Changes in develop

--- services/restaurant-service/src/app.ts
+++ services/restaurant-service/src/app.ts
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(BASE_UPLOAD_DIR));
 app.use("/", healthRoutes);
-app.use("/restaurants", restaurantRoutes);
+app.use("/", restaurantRoutes);
 app.use("/menu-categories", menuCategoryRoutes);
 app.use("/menu-items", menuItemRoutes);
 app.use("/discover", discoveryRoutes);
